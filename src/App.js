ea79af32d3c666b96c37e4e2ba466bac91ba2e32
zahangir710 <zahangir710@gmail.com>
2021-10-30 14:46:22 +0200

    routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import Headers from "./pages/Headers/Headers";
+import Footer from "./pages/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Rides from "./pages/Rides/Rides";
+import Visit from "./pages/PlanVisit/Visit";
+import YourRides from "./pages/YourRides/YourRides";
+import Login from "./pages/Login/Login";
+import NotFound from "./pages/NotFound/NotFound";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Headers></Headers>
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/rides">
+          <Rides></Rides>
+        </Route>
+        <Route path="/visit">
+          <Visit></Visit>
+        </Route>
+        <Route path="/your_rides">
+          <YourRides></YourRides>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="/profile">
+          <Profile></Profile>
+        </Route>
+        <Route exact path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
   );
 }
 
--- src/pages/Headers/Headers.js
+++ src/pages/Headers/Headers.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Headers = () => {
   return (
-    <div>
-      <h1>This is Headers</h1>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+      <Container>
+        <Navbar.Brand as={Link} to="/home">
+          Wonder World
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/rides">
+              Rides
+            </Nav.Link>
+            <Nav.Link as={Link} to="/visit">
+              Plan Your Visit
+            </Nav.Link>
+            <Nav.Link as={Link} to="/your_rides">
+              Your Rides
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/profile" eventKey={2}>
+              Profile
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
