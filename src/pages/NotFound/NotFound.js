d69176b9299a6b007010ee9f861a2cbb5ee6ae1d
zahangir710 <zahangir710@gmail.com>
2021-11-01 01:35:24 +0100

    readme file updated

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,16 @@
+import Button from "@restart/ui/esm/Button";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
+    <div className="container text-center padding">
+      <h1>404 Page Not Found</h1>
+      <Link to="/home">
+        <Button className="rounded bg-warning my-5" variant="warning">
+          Go to Home
+        </Button>
+      </Link>
     </div>
   );
 };
